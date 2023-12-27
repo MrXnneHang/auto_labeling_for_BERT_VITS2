15ac45333c6fd8f8fdd3e1bcf288ce3256e782c1
MrXnneHang <XnneHang@gmail.com>
2023-12-27 11:51:09 +0800

    添加自动化执行脚本

---
+++ 0.step1.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+from tqdm import tqdm
+from run_clip import clip 
+from run_cut import cut
+from merge_wav_by_list import merge_audio
+
+
+def get_file_list(dir):
+    file_names = os.listdir(dir)
+    if "desktop.ini" in file_names:
+        file_names.remove("desktop.ini")
+    return file_names
+def clean_txt():
+    tmp_files = os.listdir("./tmp")
+    for name in tmp_files:
+        if ".txt" in name:
+            os.remove("./tmp/"+name)
+    return tmp_files
+def clean_list():
+    files = get_file_list("./")
+    for i in files:
+        if "barbara" in i:
+            os.remove("./"+i)
+        if "long_character_anno" in i:
+            os.remove("./"+i)
+
+def main():
+    file_names = get_file_list("./raw_audio/")
+    print(file_names)
+    ask = input("请确保所有下载完的音频已经放在./raw_audio下方y/n:")
+    ## run clip
+    if ask == "y":
+        print("开始处理")
+        for i in tqdm(range(len(file_names))):
+            if len(file_names[i].split("."))>2:
+                print("请不要在音频文件命名中输入多余的.")
+                return
+            clip(wav_name=file_names[i].split(".")[0])
+            ## clip完成后删除掉这个wav
+            os.remove("./raw_audio/"+file_names[i])
+        print("All clips were done")
+    ## 移动处理完后的processed音频到raw_audio下并且清空tmp
+    tmp_files = clean_txt()
+    for name in tmp_files:
+        if ".wav" in name:
+            shutil.move("./tmp/"+name,"./raw_audio/"+name)
+    ## run_cut
+    cliped_files = get_file_list("./raw_audio")
+    for i in tqdm(range(len(cliped_files))):
+        cut(wav_name=cliped_files[i].split(".")[0])
+        os.remove("./raw_audio/"+cliped_files[i])
+    print("All cut were done")
+    cut_files = os.listdir("./tmp/cut")
+    for name in cut_files:
+        shutil.move("./tmp/cut/"+name,"./raw_audio/"+name)
+    ## 清理txt
+    clean_txt()
+    ## 清理list
+    clean_list()
+    os.system("python auto_DataLabeling_long.py")
+    os.system("python clean_list.py")
+    labeled_files = get_file_list("./raw_audio")
+    ## clean dataset folder
+    old_files = get_file_list("./dataset/processed")
+    if old_files is not None:
+        for i in old_files:
+            os.remove("./dataset/processed/"+i)
+    for i in labeled_files:
+        shutil.move("./raw_audio/"+i,"./dataset/processed/"+i)
+    with open("./clean_barbara.list","r",encoding="utf-8") as file:
+        lines = file.readlines()
+        merge_audio(lines,"./merged_wav")
+    
+    
+
+        
+if __name__ == "__main__":
+    main()
---
+++ 1.step2_after_dennoise.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+from tqdm import tqdm
+from run_clip import clip 
+from run_cut import cut
+from merge_wav_by_list import merge_audio
+
+
+def get_file_list(dir):
+    file_names = os.listdir(dir)
+    if "desktop.ini" in file_names:
+        file_names.remove("desktop.ini")
+    return file_names
+def clean_txt():
+    tmp_files = os.listdir("./tmp")
+    for name in tmp_files:
+        if ".txt" in name:
+            os.remove("./tmp/"+name)
+    return tmp_files
+def clean_list():
+    files = get_file_list("./")
+    for i in files:
+        if "barbara" in i:
+            os.remove("./"+i)
+        if "long_character_anno" in i:
+            os.remove("./"+i)
+
+def main():
+    file_names = get_file_list("./raw_audio/")
+    print(file_names)
+    ask = input("请确保所有下载完的音频已经放在./raw_audio下方y/n:")
+    new_name = input("请为你的音频起一个新名字:")
+    ## run cut
+    if ask == "y":
+        print("开始处理")
+        ## rename:
+        audios = get_file_list("./raw_audio")
+        for index,name  in enumerate(audios):
+            os.rename("./raw_audio/"+name,"./raw_audio/"+new_name+"_"+str(index)+".wav")
+        file_names = get_file_list("./raw_audio/")
+
+        ## cut前先清除所有之前的文件
+        old_files = get_file_list("./tmp/cut")
+        for old_file in old_files:
+            os.remove("./tmp/cut/"+cut_files)
+        for i in tqdm(range(len(file_names))):
+            if len(file_names[i].split("."))>2:
+                print("请不要在音频文件命名中输入多余的.")
+                return
+            cut(wav_name=file_names[i].split(".")[0])
+            os.remove("./raw_audio/"+file_names[i])
+        print("All cuts were done")
+    tmp_files = clean_txt()
+    cut_files = get_file_list("./tmp/cut/")
+    for i in cut_files:
+        shutil.move("./tmp/cut/"+i,"./raw_audio/"+i)
+    ## 清理txt
+    clean_txt()
+    ## 清理list
+    clean_list()
+    ## autolabeling
+    os.system("python auto_DataLabeling_long.py")
+    os.system("python clean_list.py")
+    labeled_files = get_file_list("./raw_audio")
+    ## clean dataset folder
+    db_dataset_path = "./db/dataset/xishi/"
+    old_files = get_file_list(db_dataset_path)
+    if old_files is not None:
+        for i in old_files:
+            os.remove(db_dataset_path+i)
+    for i in labeled_files:
+        shutil.move("./raw_audio/"+i,db_dataset_path+i)
+    shutil.move("./clean_barbara.list","./db/esd.list")
+    
+    
+
+        
+if __name__ == "__main__":
+    main()
