a1d936312300190df91119c0b2d5b63fc65aeb7b
MrXnneHang <XnneHang@gmail.com>
2024-06-23 08:44:01 +0800

    clip音频，去掉空白片段。这步后可以降噪
    Former-commit-id: 804e5c129d0c5a9d24a0823833904429debd64e4

--- single_person_step1.py
+++ single_person_step1.py
@@ -1,30 +1,52 @@
 import os
 import shutil
 from tqdm import tqdm
-from run_clip import clip 
-from run_cut import cut
-from utils.merge_wav_by_list import merge_audio
+
+
+from utils.util import get_file_list,clean_txt,load_config,read_hot_words
+from utils.time_stamp import write_long_txt ## 带有时间戳的语音识别
+from utils.short_text_to_long import convert_short_txt_to_long  ## 合并那些原本是同义句但是被拆分成多句的句子，同时也合并他们的时间线
+from utils.clip import clip_wav ## 根据新的时间线来切割我们的wav，去掉所有的空白音频
+from utils.ignore_short_sentences import ignore_short_sentence
+
+
+"""
+单人说话音频处理的第一步:
+这步会把音频中的空白背景音全部去掉，并且按照有说话的句子排列，每个句子之间间隔为1.5s
+对于不怎么健谈的游戏主播来说，这一步可能会把原本半小时的音频变成五分钟。
+这步可以减少很多降噪的时间。
+我把它叫做clip，我英文不好。
+"""
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def get_file_list(dir):
-    file_names = os.listdir(dir)
-    if "desktop.ini" in file_names:
-        file_names.remove("desktop.ini")
-    return file_names
-def clean_txt():
-    tmp_files = os.listdir("./tmp")
-    for name in tmp_files:
-        if ".txt" in name:
-            os.remove("./tmp/"+name)
-    return tmp_files
-def clean_list():
-    files = get_file_list("./")
-    for i in files:
-        if "barbara" in i:
-            os.remove("./"+i)
-        if "long_character_anno" in i:
-            os.remove("./"+i)
+
+
+def clip(wav_name):
+    """这个最终做的是什么,
+       1.生成语音字幕和time_stamp,写入第一个txt
+       2.根据*.txt合并被拆分的短句-> processed*.txt
+       3.根据processed*.txt 忽略少于一定长度的句子 -> final*.txt
+       4.根据final*.txt的time_stamp剪辑*.wav
+    """
+    config = load_config()
+    cut_line = config["cut_line"]
+    combine_line = config["combine_line"]
+    ignore_line = config["ignore_line"]
+
+    print("开始语音识别")
+    hot_words = read_hot_words()
+    write_long_txt(wav_name=wav_name, cut_line=cut_line, hot_word=hot_words)  # ./tmp/.txt
+    convert_short_txt_to_long(wav_name,combine_line=combine_line)
+    print("忽略短句")
+    ignore_short_sentence(wav_name=wav_name,audio_length=ignore_line)
+    ## 这个audio_length会把所有短于ignore_line的忽略掉，根据字幕。
+    ## 如果希望有短句，可以保留，把它设的小一点1000ms这样。
+
+    print("开始合成最终的人声合集")
+
+    clip_wav(wav_name=wav_name)
+
 
 def main():
     file_names = get_file_list("./raw_audio/")
